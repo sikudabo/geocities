eefd5e9a4fda011c6ebb8b99f32fabc2f4c41a3b
Simeon <sikudabo@alumni.iu.edu>
2021-03-03 23:46:57 -0500

    Updated the MongoDB URI to connect remotely.

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 
-const dbUri = "mongodb+srv://sikudabo:linkkup01@cluster0.zkhru.mongodb.net/tester?retryWrites=true&w=majority";
+const dbUri = 'mongodb+srv://sikudabo:shooter1@cluster0.zkhru.mongodb.net/tester?retryWrites=true&w=majority';
 
 const connection = mongoose.connect(dbUri);
 
